c616bf5223c49f1e1231593f5306a7c436868144
Stefano B <stefano.ballabeni+github@gmail.com>
2015-06-08 19:03:20 -0400

    update hr definition so only a series of the same character matches an hr

--- lib/commonmark/parser.rb
+++ lib/commonmark/parser.rb
@@ -5,9 +5,12 @@
 
     rule(:cr)         { match('\n') }
     rule(:eol?)       { cr | any.absent?  }
-    rule(:line_body)  { (eol?.absent? >> any).repeat(1) }
+    rule(:line_body)  { (eol?.absent? >> (emphasize | any)).repeat(1) }
     rule(:line)       { cr | (line_body >> eol?) }
     rule(:text) { line.as(:text) }
+
+
+    rule(:emphasize) { str('*') >> any.repeat.as(:emphasize) >> str('*') }
 
     rule(:paragraph_line) { (line_body).as(:paragraph_line) >> eol? }
 
@@ -17,17 +20,19 @@
     rule(:space) { match(' ') }
     rule(:tab) { match('\t').repeat(1) }
 
-    rule(:hr_char) { str('-') | str('*') | str('_') }
+    rule(:hr_star) { (str('*') >> hr_delimiter.repeat).repeat(3) }
+    rule(:hr_dash) { (str('-') >> hr_delimiter.repeat).repeat(3) }
+    rule(:hr_underscore) { (str('_') >> hr_delimiter.repeat).repeat(3) }
     rule(:hr_delimiter) { space }
+    rule(:hr_chars) { hr_star | hr_dash | hr_underscore }
 
-    rule(:hr) { space.repeat(0, 3) >> (hr_char >> hr_delimiter.repeat).repeat(3).as(:hr) >> eol? }
+    rule(:hr) { space.repeat(0, 3) >> (hr_chars).as(:hr) >> eol? }
 
     rule(:unordered_list_item) { dash >> space >> (line_body).as(:li) >> eol?}
     rule(:unordered_list) { unordered_list_item.repeat(1).as(:ul) }
 
     rule(:code_line) { space.repeat(4) >> (line_body >> eol?).as(:code_line) }
     rule(:code_block) { code_line.repeat(1).as(:code_block) }
-
 
     rule(:markdown) { (hr | unordered_list | code_block | paragraph ).repeat(1) }
 
@@ -51,6 +56,7 @@
 
     rule(hr: simple(:hr)) { "<hr />\n" }
 
+    rule(emphasize: simple(:text)) { "<em>#{text}</em>" }
     rule(paragraph_line: simple(:line)) { line }
     rule(paragraph: sequence(:paragraph_lines)) { "<p>#{paragraph_lines.join("\n")}</p>" }
   end
